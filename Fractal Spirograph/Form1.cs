b3464cb2776b07b9d0afb0f8d872cf3e7b652759
Jakub Kajfosz <kajf.jakub@gmail.com>
2017-03-11 16:43:46 +0100

    Performance improvements.

--- Fractal Spirograph/Form1.cs
+++ Fractal Spirograph/Form1.cs
@@ -24,46 +24,56 @@ namespace Fractal_Spirograph
         static int width, height;
         static Random rnd = new Random();
         static long frameCount = 0;
-        List<PVector> path = new List<PVector>();
 
         Circle sun;
         Circle end;
+        Circle begin;
+        Circle last = new Circle(0, 0, 0, 0);
+        bool endDraw = false;
 
         const int k = -4;
+        float shortest = 50.0F;
+        bool drawCircles = false;
+        string dateAndTimeWhenProgramStarted;
+
+        List<PVector> path = new List<PVector>();
+
+        public static float map(float value, float valueMin, float valueMax, float targetMin, float targetMax)
+        {
+            return (float)Math.Floor((((value - valueMin) / (double)(valueMax - valueMin))) * (targetMax - targetMin) + targetMin);
+        }
 
         class Circle
         {
-            public float x;
-            public float y;
-            public float r;
-            public float angle;
-            public float speed;
+            public PVector pos;
+            public double r;
+            public double angle;
+            public double speed;
             public int n;
             public Circle parent;
             public Circle child;
 
-            public Circle(float x, float y, float r, int n) : this(x, y, r, n, null)
+            public Circle(float x, float y, double r, int n) : this(x, y, r, n, null)
             {
                 
             }
 
-            public Circle(float x, float y, float r, int n, Circle parent)
+            public Circle(float x, float y, double r, int n, Circle parent)
             {
-                this.x = x;
-                this.y = y;
+                pos = new PVector(x, y);
                 this.r = r;
                 this.n = n;
                 this.parent = parent;
-                angle = 0;
-                speed = (float)(Math.Pow(k, n - 1) * Math.PI / 180.0F) / 10.0F;
+                angle = 0.0;
+                speed = (Math.Pow(-10, n - 1)  * Math.PI / 180.0 / (Math.Pow(2.8, Math.Abs(k - 1))));
             }
 
             public Circle addChild()
             {
-                float newr = r / 3.0F;
-                float newx = x + r + newr;
-                float newy = y;
-                child = new Circle(newx, newy, newr, n + 1, this);
+                double newr = r / 3.0F;
+                double newx = pos.x + r + newr;
+                double newy = pos.y;
+                child = new Circle((float)newx, (float)newy, newr, n + 1, this);
                 return child;
             }
 
@@ -72,20 +82,21 @@ namespace Fractal_Spirograph
                 if(parent != null)
                 {
                     angle += speed;
-                    float rsum = r + parent.r;
-                    x = parent.x + rsum * (float)Math.Cos(angle);
-                    y = parent.y + rsum * (float)Math.Sin(angle);
+                    double rsum = r + parent.r;
+                    pos.x = parent.pos.x + (float)(rsum * Math.Cos(angle));
+                    pos.y = parent.pos.y + (float)(rsum * Math.Sin(angle));
                 }
             }
 
             public void show()
             {
-                g.DrawEllipse(Pens.Gray, x - r, y - r, r * 2.0F, r * 2.0F);
+                g.DrawEllipse(Pens.Gray, pos.x - (float)r, pos.y - (float)r, (float)r * 2.0F, (float)r * 2.0F);
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dateAndTimeWhenProgramStarted = DateTime.Today.ToString("yyyy-MM-dd") + "_" + DateTime.Now.ToString("HH-mm-ss");
             pictureBox1.Width = Form1.ActiveForm.Width - 16;
             pictureBox1.Height = Form1.ActiveForm.Height - 39;
             width = pictureBox1.Width;
@@ -93,46 +104,77 @@ namespace Fractal_Spirograph
             bmp = new Bitmap(width, height);
             g = Graphics.FromImage(bmp);
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            g.Clear(Color.White);
+            g.Clear(Color.Black);
             pictureBox1.Image = bmp;
 
             sun = new Circle(width / 2, height / 2, 150, 0);
             Circle current = sun;
-            for(int i = 0; i < 10; i++)
+            for(int i = 0; i < 5; i++)
             {
                 current = current.addChild();
             }
-            end = current;
+            begin = new Circle(current.pos.x, current.pos.y, current.r, current.n);
         }
-
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            g.Clear(Color.Black);
-
             Circle current = sun;
             while(current != null)
             {
                 current.update();
-                //current.show();
+                end = current;
                 current = current.child;
             }
 
-            path.Add(new PVector(end.x, end.y));
 
-            if (frameCount % 5 == 0)
+            path.Add(new PVector(end.pos.x, end.pos.y));
+            if (frameCount > 0)
             {
-                for (int i = 1; i < path.Count; i++)
+                if(drawCircles)
                 {
-                    g.DrawLine(Pens.MediumPurple, path[i - 1].x, path[i - 1].y, path[i].x, path[i].y);
+                    g.Clear(Color.Black);
+                    current = sun;
+                    while (current != null)
+                    {
+                        current.show();
+                        current = current.child;
+                    }
+                    for (int i = 1; i < path.Count; i++)
+                    {
+                        g.DrawLine(Pens.MediumPurple, path[i - 1].x, path[i - 1].y, path[i].x, path[i].y);
+                    }
                 }
+                else
+                {
+                    g.DrawLine(Pens.MediumPurple, last.pos.x, last.pos.y, end.pos.x, end.pos.y);
+                }
+
+                if (frameCount % 3 == 0)
+                {
+                    pictureBox1.Image = bmp;
+                    pictureBox1.Update();
+                }
+                //g.DrawString(shortest.ToString(), DefaultFont, Brushes.White, width - 100, height - 40);
+            }
+
+            
+
+            last.pos = end.pos.copy();
+            if(PVector.dist(begin.pos, end.pos) < 4.0F)
+            {
+                shortest = (float)sun.child.angle % (float)(Math.PI * 2.0F);
+            }
+            if(endDraw && sun.child.angle % (Math.PI * 2.0F) > 0.01F && sun.child.angle % (Math.PI * 2.0F) < Math.PI * 2.0F - 0.01F)
+            {
+                timer1.Stop();
+                bmp.Save(".\\Images\\" + dateAndTimeWhenProgramStarted + "_SpirographImage.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                g.DrawString("Done", DefaultFont, Brushes.White, width - 100, height - 40);
                 pictureBox1.Image = bmp;
                 pictureBox1.Update();
             }
-
-            if(frameCount > 50 && sun.child.angle % (Math.PI * 2.0F) < 0.01F)
+            if (frameCount > 400 && (sun.child.angle % (Math.PI * 2.0F) < 0.2F || sun.child.angle % (Math.PI * 2.0F) > Math.PI * 2.0F - 0.2F) && PVector.dist(begin.pos, end.pos) < 4.0F)
             {
-                timer1.Stop();
-                bmp.Save("Spirograph.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                endDraw = true;
             }
 
             frameCount++;
